ab2be92d653a69a2cac3db9a5e697a806a4b79ef
Abs-11001 <1196399230@qq.com>
2023-03-07 23:07:22 +0800

    抽取plate子组件，完成main区四大板块设置

--- src/pages/home/TheHome.vue
+++ src/pages/home/TheHome.vue
@@ -1,98 +1,47 @@
 <template>
   <div class="width90 main">
     <el-row :gutter="10">
-      <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
+      <el-col :xs="24" :sm="12" :md="8" :lg="6" :xl="6">
         <el-skeleton :rows="6" animated :throttle="500" :loading="loading" >
-          <div class="plate new_thread">
-            <div class="container">
-              <div class="title_box">
-                <h5 class="title">新鲜出炉(更多)</h5>
-              </div>
-              <div class="content_box">
-                <template v-for="index of 10"  :key="index">
-                  <div class="thread_line">
-                    <a href="http://www.waheng.fun">三分导航三分导航三分导航i love you s</a>
-                  </div>
-                </template>
-              </div>
-            </div>
-          </div>
+          <PlateItem title="新鲜出炉(更多)"></PlateItem>
         </el-skeleton>
       </el-col>
 
-      <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
+      <el-col :xs="24" :sm="12" :md="8" :lg="6" :xl="6">
         <el-skeleton :rows="6" animated :throttle="500"  :loading="loading" >
-          <div class="plate hot_thread">
-            <div class="container">
-              <div class="title_box">
-                <h5 class="title">人气热门(更多)</h5>
-              </div>
-            </div>
-          </div>
+          <PlateItem title="人气热门(更多)"></PlateItem>
         </el-skeleton>
       </el-col>
 
-      <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
+      <el-col :xs="24" :sm="12" :md="8" :lg="6" :xl="6">
         <el-skeleton :rows="6" animated :throttle="500"  :loading="loading" >
-          <div class="plate evil_thread">
-            <div class="container">
-              <div class="title_box">
-                <h5 class="title">灌水嫌疑(更多)</h5>
-              </div>
-            </div>
-          </div>
+          <PlateItem title="疑似灌水(更多)"></PlateItem>
+        </el-skeleton>
+      </el-col>
+
+      <el-col :xs="24" :sm="12" :md="8" :lg="6" :xl="6">
+        <el-skeleton :rows="6" animated :throttle="500"  :loading="loading" >
+          <PlateItem title="疑似灌水(更多)"></PlateItem>
         </el-skeleton>
       </el-col>
     </el-row>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'TheHome',
-  data(){
-    return {
-      loading: true
-    }
-  },
-  mounted(){
-    setTimeout(()=>{this.loading = false}, 1000)
-  }
-}
+<script setup>
+import { ref, onMounted } from 'vue'
+import PlateItem from "@/pages/home/plate/PlateItem.vue";
+
+const loading = ref(true)
+
+onMounted(() => {
+    setTimeout(()=>{loading.value = false}, 1000)
+})
+
 </script>
 
 <style lang="less" scoped>
   .main{
-    .plate{
-      //border: 1px solid red;
-      .container{
-        .title_box{
-          //border: 1px solid blue;
-          .title{
-            padding: 5px 0;
-            text-align: center;
-            background-color:#f2f2f2;
-          }
-        }
-        .content_box{
-          background-image: url(./img/list_bg.gif);
-          background-repeat: no-repeat;
-          padding-left: 20px;
-          .thread_line{
-            margin-left: 5px;
-            height: 22px;
-            line-height: 22px;
-            overflow: hidden;
-            white-space:nowrap;
-            text-overflow: ellipsis;
-            border-bottom: 1px dashed #CDCDCD;
-            a{
-              text-decoration: none;
-              color: black;
-            }
-          }
-        }
-      }
-    }
+    margin-top: 10px;
   }
 </style>
---
+++ src/pages/home/plate/PlateItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="plate">
+    <div class="container">
+      <div class="title-box">
+        <h5 class="title">{{ title }}</h5>
+      </div>
+      <div class="content-box">
+        <template v-for="index of 10"  :key="index">
+          <div class="content-item">
+            <a href="http://www.waheng.fun">三分导航三分导航三分导航i love you s</a>
+          </div>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    title: String
+  })
+</script>
+
+<style lang="less" scoped>
+  .plate{
+  //border: 1px solid red;
+  .container{
+    .title-box{
+      //border: 1px solid blue;
+      .title{
+        padding: 5px 0;
+        text-align: center;
+        background-color:#f2f2f2;
+      }
+    }
+    .content-box{
+      background-image: url(../img/list_bg.gif);
+      background-repeat: no-repeat;
+      padding-left: 20px;
+      .content-item{
+        margin-left: 5px;
+        height: 22px;
+        line-height: 22px;
+        overflow: hidden;
+        white-space:nowrap;
+        text-overflow: ellipsis;
+        border-bottom: 1px dashed #CDCDCD;
+        a{
+          text-decoration: none;
+          color: black;
+        }
+      }
+    }
+  }
+}
+</style>
