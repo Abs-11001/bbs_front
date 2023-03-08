7d49b37a60939511c0074642ecedc29f96bc131a
Abs-11001 <1196399230@qq.com>
2023-03-09 00:41:51 +0800

    使用媒体查询进行页面动态适配

--- src/components/search/TheSearch.vue
+++ src/components/search/TheSearch.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="width90">
+  <div class="search-content width90">
     <div class="search-box">
-      <el-input
-          v-model="search"
-          class="w-50 m-2"
-          placeholder="请输入要查询的内容"
-          :suffix-icon="Search"/>
+      <div class="search-input">
+        <el-input
+            v-model.trim="search"
+            class="w-50 m-2"
+            clearable
+            placeholder="请输入要查询的内容">
+          <template #append>
+            <el-button :icon="Search"/>
+          </template>
+        </el-input>
+      </div>
+      <div class="hot-words">
+        <span>热搜：</span>
+        <el-link type="primary" target="_blank">我是校园</el-link>
+        <el-link type="primary" target="_blank">暨阳致用</el-link>
+        <el-link type="primary" target="_blank">我是校园</el-link>
+        <el-link type="primary" target="_blank">暨阳致用</el-link>
+        <el-link type="primary" target="_blank">我是校园</el-link>
+      </div>
     </div>
   </div>
 </template>
@@ -18,7 +32,32 @@
 </script>
 
 <style lang="less" scoped>
-  .search-box{
-    width: 300px;
+  .search-content{
+    margin-top: 5px;
+    .search-box{
+      display: flex;
+      .search-input{
+        width: 30%;
+      }
+      .hot-words{
+        display: flex;
+        align-items: center;
+        margin-left: 15px;
+        span{
+          font-weight: 700;
+        }
+        .el-link{
+          margin: 0 5px;
+        }
+      }
+      @media screen and (max-width: 480px){
+        .hot-words{
+          display: none;
+        }
+        .search-input{
+          width: 60%;
+        }
+      }
+    }
   }
 </style>
