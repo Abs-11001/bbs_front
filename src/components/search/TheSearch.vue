460bd5abc9d249fe8d552eaadbaf5cea5c232cb5
Abs-11001 <1196399230@qq.com>
2023-03-12 23:47:59 +0800

    完善搜索组件的显示及逻辑，添加通知轮播，添加820px媒体查询，设置屏幕最大宽度为1440px

--- src/components/search/TheSearch.vue
+++ src/components/search/TheSearch.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="search-content width90">
-    <div class="search-box">
+  <div class="search-content quick-news width90">
+    <div class="left">
       <div class="search-input">
         <el-input
             v-model.trim="search"
             class="w-50 m-2"
             clearable
-            placeholder="请输入要查询的内容">
+            placeholder="请输入要搜索的内容">
           <template #append>
             <el-button :icon="Search"/>
           </template>
@@ -14,48 +14,108 @@
       </div>
       <div class="hot-words">
         <span>热搜：</span>
-        <el-link type="primary" target="_blank">我是校园</el-link>
-        <el-link type="primary" target="_blank">暨阳致用</el-link>
-        <el-link type="primary" target="_blank">我是校园</el-link>
-        <el-link type="primary" target="_blank">暨阳致用</el-link>
-        <el-link type="primary" target="_blank">我是校园</el-link>
+        <a href="http://www.waheng.fun" target="_blank" title="暨阳1212121212致用啊">{{ hotWordsFilter('暨阳1212121212致用啊') }}</a>
+        <a href="http://www.waheng.fun" target="_blank" title="听">{{ hotWordsFilter('听') }}</a>
+        <a href="http://www.waheng.fun" target="_blank" title="暨阳致用">{{ hotWordsFilter('暨阳致用') }}</a>
+        <a href="http://www.waheng.fun" target="_blank" title="校园">{{ hotWordsFilter('校园') }}</a>
+      </div>
+    </div>
+    <div class="right">
+      <div class="news">
+        <el-carousel height="30px" direction="vertical" :autoplay="true" interval="2500">
+          <el-carousel-item v-for="n in news" :key="n.idx">
+<!--            <h3 text="2xl" justify="center">{{ n.title }}</h3>-->
+            <a :href="n.url" :title="n.title">{{ n.title }}({{ n.time }})</a>
+          </el-carousel-item>
+        </el-carousel>
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
-  import { ref } from 'vue'
+<script setup lang="ts">
+  import { computed, ref, reactive } from 'vue'
   import { Search } from '@element-plus/icons-vue'
 
   const search = ref('')
+  const news = reactive([
+    {
+      idx: 1,
+      title: '人文学院、马克思主义学院赴诸暨市文化广电旅游局开展访企拓岗促就业专项行动',
+      url: 'http://www.waheng.fun',
+      time: '2023-03-12'
+    },
+    {
+      idx: 2,
+      title: '关于2023届省级优秀毕业生及院级优秀毕业生推荐名单的公示',
+      url: 'http://www.waheng.fun',
+        time: '2023-03-12'
+    },
+    {
+      idx: 3,
+      title: '商学院2023届绍兴市优秀毕业生 评比结果公示',
+      url: 'http://www.waheng.fun',
+        time: '2023-03-12'
+    },
+  ])
+
+  function hotWordsFilter(hotWord: String) {
+    // 截取热搜的前四位关键字
+    let suffix = ''
+    if(hotWord.length > 4) suffix = '...'
+    return hotWord.substring(0, 4) + suffix
+  }
 </script>
 
 <style lang="less" scoped>
   .search-content{
+    display: flex;
+    justify-content: space-between;
     margin-top: 5px;
-    .search-box{
+    // 在media_max_width_820px.less中，实现了媒体查询功能，修改时注意代码适配
+    .left{
+      flex: 0 1 40%;
       display: flex;
       .search-input{
-        width: 30%;
+        flex: 1 1 50%;
       }
       .hot-words{
-        display: flex;
-        align-items: center;
+        flex: 1 1 40%;
         margin-left: 15px;
+        overflow: hidden;
+        white-space: nowrap;
+        font-size: 0.8rem;
         span{
+          line-height: 32px;
           font-weight: 700;
         }
-        .el-link{
+        a{
           margin: 0 5px;
+          overflow: hidden;
+          white-space: nowrap;
+          text-align: center;
+          &:hover{
+            color: #79bbff;
+            border-bottom: 2px solid #79bbff;
+          }
         }
       }
-      @media screen and (max-width: 480px){
-        .hot-words{
-          display: none;
-        }
-        .search-input{
-          width: 60%;
+    }
+    .right{
+      flex: 0 1 40%;
+      .news{
+        .el-carousel{
+          a{
+            font-size: 0.9rem;
+            &:hover{
+              color:  #95d475;
+            }
+          }
+          ::v-deep .el-carousel__item{
+            display: flex;
+            flex-direction: row-reverse;
+            align-items: center;
+          }
         }
       }
     }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import '@/assets/css/base.css'
+import '@/assets/css/media_max_width_820px.less'
 
 const app = createApp(App)
 app.use(router)
