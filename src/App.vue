b17b99c387013303051f1b7f6ab6ebfed228918b
Abs-11001 <1196399230@qq.com>
2023-04-16 14:35:33 +0800

    添加页脚部分和回到顶部组件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script setup>
   import TheHeader from "./components/header/TheHeader.vue";
   import TheNav from "./components/nav/TheNav.vue";
+  import TheCarousel from "@/components/carousel/TheCarousel.vue";
   import TheSearch from "@/components/search/TheSearch.vue";
-  import BreadCrumb from "@/components/breadCrumb/BreadCrumb.vue";
+  import TheFooter from "@/components/footer/TheFooter.vue";
 </script>
 
 <template>
     <the-header/>
     <the-nav/>
     <the-search/>
-    <bread-crumb/>
+    <the-carousel/>
     <router-view></router-view>
+    <the-footer/>
+    <el-backtop :right="100" :bottom="100" />
 </template>
 
 <style>
---
+++ src/components/footer/TheFooter.vue
@@ -0,0 +1,83 @@
+<template>
+  <footer>
+    <div class="container width90">
+      <div class="info">
+        <el-image :src='bbs_logo'/>
+        <p>一个专门为暨阳打造的校园论坛系统</p>
+      </div>
+      <div class="part">
+        <div class="part-title">网站相关</div>
+        <ul>
+          <li>
+            <el-link href="http://www.waheng.fun" target="_blank">常见问题</el-link>
+          </li>
+          <li>
+            <el-link href="http://www.waheng.fun" target="_blank">规章制度</el-link>
+          </li>
+          <li>
+            <el-link href="http://www.waheng.fun" target="_blank">免责申明</el-link>
+          </li>
+        </ul>
+      </div>
+      <div class="part">
+        <div class="part-title">友情链接</div>
+        <ul>
+          <li>
+            <el-link href="http://www.waheng.fun" target="_blank">浙江农林大学暨阳学院</el-link>
+          </li>
+          <li>
+            <el-link href="http://www.waheng.fun" target="_blank">百度</el-link>
+          </li>
+          <li>
+            <el-link href="http://www.waheng.fun" target="_blank">浙江农林大学</el-link>
+          </li>
+        </ul>
+      </div>
+      <div class="part">
+        <div class="part-title">网站相关</div>
+        <ul>
+          <li>
+            <el-link href="http://www.waheng.fun" target="_blank">常见问题</el-link>
+          </li>
+          <li>
+            <el-link href="http://www.waheng.fun" target="_blank">规章制度</el-link>
+          </li>
+          <li>
+            <el-link href="http://www.waheng.fun" target="_blank">免责申明</el-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+
+  const bbs_logo = new URL('../../assets/images/bbs_logo.png', import.meta.url).href
+
+</script>
+
+<style lang="less" scoped>
+  footer{
+    display: flex;
+    align-items: center;
+    height: 150px;
+    margin-top: 20px;
+    background-color: #e9e9e9;
+    .container{
+      display: flex;
+      justify-content: space-around;
+      .info{
+        .el-image{
+          width: 200px;
+        }
+        font-size: 0.9rem;
+      }
+      .part{
+        ul{
+          list-style: none;
+        }
+      }
+    }
+  }
+</style>
