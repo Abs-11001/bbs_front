7d7ca747fc27224fc097cb484867d3d1ed3f57a1
Abs-11001 <1196399230@qq.com>
2023-04-27 02:44:59 +0800

    修改项目默认端口号

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # bbs_front
 
+校园论坛前台的前端项目
+
 This template should help get you started developing with Vue 3 in Vite.
 
 ## Recommended IDE Setup
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
+        host:'0.0.0.0' ,//ip地址
+        port: 80, // 设置服务启动端口号
+        open: true, // 设置服务启动时是否自动打开浏览器
         proxy: {
             "/api": {
                 target: "http://localhost:8080",
@@ -26,8 +29,8 @@ export default defineConfig({
       }),
     ],
     resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-    }
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
 })
