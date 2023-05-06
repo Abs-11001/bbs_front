27dfa51de6c7754a946bbc0d90c9677f339a904f
waheng <1196399230@qq.com>
2023-05-06 11:51:12 +0800

    优化搜索结果

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="containter width90">
-    <h2>搜索结果</h2>
+  <div class="container width90">
+    <h2 class="title">关于<span class="key">{{key}}</span>相关搜索结果</h2>
+    <p class="total">共有{{ total }}条</p>
     <el-descriptions
         v-for="(item, index) in dataList"
         direction="vertical"
@@ -15,13 +16,25 @@
       <el-descriptions-item label="所属板块" width="200px" align="center">{{ item.category }}</el-descriptions-item>
       <el-descriptions-item label="发布时间" width="200px" align="center">{{ item.addTime }}</el-descriptions-item>
     </el-descriptions>
+    <el-config-provider :locale="zhCn">
+      <el-pagination
+          background
+          layout="prev, pager, next"
+          v-model:current-page="page"
+          :total="total"
+          @current-change="getData"
+          @prev-click="getData"
+          @next-click="getData"/>
+    </el-config-provider>
   </div>
 </template>
 
 <script setup>
-import {onMounted, ref} from "vue";
+import {onMounted, reactive, ref} from "vue";
 import {useRouter, useRoute} from "vue-router";
 import {searchAll} from "@/api/search";
+import zhCn from "element-plus/lib/locale/lang/zh-cn";
+import {ElConfigProvider} from "element-plus";
 const route = useRoute()
 const router = useRouter()
 
@@ -29,19 +42,19 @@ onMounted(() => {
   getData()
 })
 const dataList = ref()
+const page = ref(1)
+const total = ref(0)
+const key = route.query.key
 function getData() {
-  const key = route.query.key
-  const page = route.query.page
-  const limit = route.query.limit
   const query = {
     key,
-    page,
-    limit
+    page: page.value
   }
   searchAll(query).then(res => {
     const { code, data } = res
     if(code === 200) {
-      dataList.value = data
+      dataList.value = data.dataList
+      total.value = data.total
     }
   })
 }
@@ -61,6 +74,19 @@ function goToDetail(item) {
 </script>
 
 <style lang="less" scoped>
+  .title{
+    font-size: 20px;
+    color: #606266;
+    .key{
+      font-size: 2rem;
+      color: black;
+    }
+  }
+  .total{
+    font-size: 12px;
+    color: #999;
+    margin-bottom: 5px;
+  }
   .el-descriptions{
     margin-bottom: 15px;
   }
